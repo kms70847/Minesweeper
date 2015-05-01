a56a5dba5d3e94a6072e963b5151f6bfbf26f4f6
kms70847 <kevin.m.smiley@gmail.com>
2015-05-01 09:44:59 -0400

    initial implementation of chain reaction uncovering

--- display/stateView.py
+++ display/stateView.py
@@ -4,10 +4,7 @@ class StateView(ImageGrid):
     def __init__(self, root, state, **kwargs):
         self.state = state
         
-        kwargs["names"] = {
-            "covered": "images/img.png",
-            "uncovered": "images/img2.gif"
-        }
+        kwargs["names"] = {name: "images/{}.png".format(name) for name in "covered uncovered 1 2 3 4 5 6 7 8 flagged unsure".split()}
         kwargs["rows"] = state.height
         kwargs["cols"] = state.width
         kwargs["default"] = "covered"
@@ -18,4 +15,7 @@ class StateView(ImageGrid):
         if state == "up":
             if not (self.in_range(pos) and pos == last_down_pos):
                 return
-            print "clicked at {}".format(pos)
+            if self.state.cell_states[pos] == self.state.covered:
+                for cell in self.state.get_group(pos):
+                    self.state.cell_states[cell] = self.state.uncovered
+                    self.set_image(cell, self.state.get_name(cell))
