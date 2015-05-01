7260095e93e3e46dcc772c594ff29fb984531a8e
kms70847 <kevin.m.smiley@gmail.com>
2015-05-01 09:35:14 -0400

    Test StateView instead of ImageGrid

--- display/test.py
+++ display/test.py
@@ -1,23 +1,15 @@
 from Tkinter import *
-from imageGrid import ImageGrid
 
-def clicked(event, pos, button, state, last_down_pos):
-    #print pos, button, state, last_down_pos
-    if button == "left":
-        if state == "down":
-            x.set_image(pos[0], pos[1], "uncovered")
-        elif state == "up":
-            if pos != last_down_pos:
-                #user moved cursor out of cell before unpressing. This means he wants to cancel.
-                x.set_image(last_down_pos[0], last_down_pos[1], "covered")
-            else:
-                print "Permanently uncovering {} {}...".format(*pos)
+from stateView import StateView
 
+import sys
+import os.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from state import State
+
+
+state = State(10, 10, 5)
 root = Tk()
-
-names = {"covered": "img.png", "uncovered": "img2.gif"}
-x = ImageGrid(root, rows=10, cols=10, names=names, default="covered")
+x = StateView(root, state)
 x.pack()
-x.bind_cell(clicked)
-
 root.mainloop()
