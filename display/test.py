1e7cf66565edd4d57e15a57b1f64c5e9ac46030b
kms70847 <kevin.m.smiley@gmail.com>
2015-05-01 10:10:16 -0400

    Test on a larger grid

--- display/test.py
+++ display/test.py
@@ -7,8 +7,10 @@ import os.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from state import State
 
-
-state = State(10, 10, 5)
+#beginner: 9,9,10
+#intermediate: 16, 16, 40
+#advanced: 16, 30, 99
+state = State(16, 16, 40)
 root = Tk()
 x = StateView(root, state)
 x.pack()
