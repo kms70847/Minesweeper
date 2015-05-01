18ce61951413aca6fa2872c6483db30bf6b91635
kms70847 <kevin.m.smiley@gmail.com>
2015-05-01 09:35:04 -0400

    Created StateView class

---
+++ display/stateView.py
@@ -0,0 +1,21 @@
+from imageGrid import ImageGrid
+
+class StateView(ImageGrid):
+    def __init__(self, root, state, **kwargs):
+        self.state = state
+        
+        kwargs["names"] = {
+            "covered": "img.png",
+            "uncovered": "img2.gif"
+        }
+        kwargs["rows"] = state.height
+        kwargs["cols"] = state.width
+        kwargs["default"] = "covered"
+        ImageGrid.__init__(self, root, **kwargs)
+
+        self.bind_cell(self.clicked)
+    def clicked(self, event, pos, button, state, last_down_pos):
+        if state == "up":
+            if not (self.in_range(pos) and pos == last_down_pos):
+                return
+            print "clicked at {}".format(pos)
