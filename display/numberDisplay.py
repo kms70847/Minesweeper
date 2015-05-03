19fc36b018135b8f9a04f9ee7098ec19185f7796
kms70847 <kevin.m.smiley@gmail.com>
2015-05-03 10:03:24 -0400

    NumberDisplay, a widget that displays numbers

---
+++ display/numberDisplay.py
@@ -0,0 +1,30 @@
+from imageGrid import ImageGrid
+from Tkinter import *
+from geometry import Point
+
+class NumberDisplay(ImageGrid):
+    def __init__(self, root, digits=3):
+        names = {str(x): "images/big_{}.png".format(x) for x in range(10)}
+        names["empty"] = "images/big_empty.png"
+        ImageGrid.__init__(self, root, names=names, rows=1, cols=digits, default="empty")
+        self.digits = digits
+        self.max_value = 10**self.digits - 1
+        self.value = None
+    def get(self):
+        return self.value
+    def set(self, value):
+        self.value = value
+        if value is None:
+            for i in range(self.digits):
+                self.set_image(Point(i,0), "empty")
+        elif isinstance(value, int):
+            assert value >= 0
+            s = str(value)
+            assert len(s) <= self.digits
+            while len(s) < self.digits:
+                s = " " + s
+            for idx, c in enumerate(s):
+                name = "empty" if c == " " else c
+                self.set_image(Point(idx,0), name)
+        else:
+            raise ValueError("value may only be integer or None")
