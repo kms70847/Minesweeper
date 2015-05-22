8db9d1933bb3fed9caf7a14e092302e3245ab0b9
kms70847 <kevin.m.smiley@gmail.com>
2015-05-22 09:57:45 -0400

    Allow negative numbers in `numberDisplay`

--- display/numberDisplay.py
+++ display/numberDisplay.py
@@ -6,6 +6,7 @@ class NumberDisplay(ImageGrid):
     def __init__(self, root, digits=3):
         names = {str(x): "images/big_{}.png".format(x) for x in range(10)}
         names["empty"] = "images/big_empty.png"
+        names["minus"] = "images/big_minus.png"
         ImageGrid.__init__(self, root, names=names, rows=1, cols=digits, default="empty")
         self.digits = digits
         self.max_value = 10**self.digits - 1
@@ -18,13 +19,12 @@ class NumberDisplay(ImageGrid):
             for i in range(self.digits):
                 self.set_image(Point(i,0), "empty")
         elif isinstance(value, int):
-            assert value >= 0
             s = str(value)
             assert len(s) <= self.digits
             while len(s) < self.digits:
                 s = " " + s
             for idx, c in enumerate(s):
-                name = "empty" if c == " " else c
+                name = {" ": "empty", "-": "minus"}.get(c,c)
                 self.set_image(Point(idx,0), name)
         else:
             raise ValueError("value may only be integer or None")
