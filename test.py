8f05acb4e46cbb6be56112e4ab4e41ec5f55d377
kms70847 <kevin.m.smiley@gmail.com>
2015-05-23 15:15:34 -0400

    Move contents of `display` to main directory

--- test.py
+++ test.py
@@ -8,9 +8,6 @@ from settingsWindow import SettingsWindow
 from numberDisplay import NumberDisplay
 import highscores
 
-import sys
-import os.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from state import State
 
 class ImageButton(Button):
